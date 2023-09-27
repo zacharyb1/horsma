bf50f808b645ec7f8faaa4ecff7de63a9a12b02c
ZacharyB1 <zachary.burda@gmail.com>
2023-09-27 16:35:56 +0300

    before list fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,14 +15,16 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <p className="text-white text-center">Miksi tarvitset</p>
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
-        Laadukkaat kotisivut ja digipalvelut yrityksellesi
+        Laadukkaat kotisivut ja digipalvelut yrityksellesi?
         </h1>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <p className="text-white text-center">Koska</p>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4  ">
           <h3 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
             Modernit kotisivut palvelevat kävijöitä paremmin ja tuottavat enemmän kauppaa
           </h3>
-          <p className="text-white text-center">
+          <p className="text-2xl text-white text-center">
             Lähes jokainen ostoprosessi verkossa alkaa Googlesta. Yrityksen kotisivut tavoittavat asiakkaat ennen yrityksen henkilökuntaa. Valitse kotisivuillesi ammattitekijä ja laita yrityksesi julkisivu ja kassavirta kuntoon. Nopea, näyttävä ja hakukoneoptimoitu WordPress-verkkosivusto tehostaa yrityksen myyntiä ja vahvistaa brändiä. Hanki kotisivut yritykselle helposti ja vaivattomasti Horsmalta.
           </p>
           <div className="text-center text-white">
@@ -31,22 +33,15 @@ export default function Home() {
             <p>Sopii kiireiselle yrittäjälle, joka ei halua käyttää paljon aikaa projektiin</p>
             <p>Haluat löytyä Googlesta ja ohjata asiakkaat yhteydenottoon</p>
           </div>
-          <form className="flex flex-col items-center mt-8">
-            <label htmlFor="email" className="text-white mb-2 text-center">
-              Haluatko saada tietoa tulevista kampanjoista?
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Sähköpostiosoite"
-              className="border border-white rounded-md px-4 py-2 text-black mb-4"
-            />
-            <button className="bg-white text-black px-4 py-2 rounded-md">Tilaa uutiskirje</button>
-          </form>
+          
         </div>
       
-        
+
+      
+      
+      
+
+      
       <p className="text-2xl text-white">
         Ota yhteyttä!
       </p>
@@ -71,12 +66,7 @@ export default function Home() {
   <button className="bg-white text-black px-4 py-2 rounded-md">Tilaa</button>
 </form>
       
-
-
-
-          
-          
-            
+  
         </div>
       </main>
     </>
