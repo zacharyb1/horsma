c2fc24be8a9ac3f85295f580f8871d28d33a7ca7
ZacharyB1 <zachary.burda@gmail.com>
2023-09-29 11:48:34 +0300

    nav updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,22 +16,22 @@ export default function Home() {
       </Head>
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 
-      <nav className="bg-gray-900 w-full">
-  <ul className="flex justify-end items-right h-full ml-auto">
-    <li>
-      <a href="verkkosivut" className="text-white font-bold text-lg">Verkkosivut</a>
-    </li>
-    <li>
-      <a href="verkkokaupat" className="text-white font-bold text-lg">Verkkokaupat</a>
-    </li>
-    <li>
-      <a href="mobiilisovellukset" className="text-white font-bold text-lg">Mobiilisovellukset</a>
-    </li>
-    <li>
-      <a href="yhteystiedot" className="text-white font-bold text-lg">Yhteystiedot</a>
-    </li>
-  </ul>
-</nav>
+    <nav className="bg-gray-900 w-full">
+      <ul className="flex justify-end items-right h-full ml-auto">
+        <li>
+          <a href="verkkosivut" className="text-white font-bold text-lg">Verkkosivut</a>
+        </li>
+        <li>
+          <a href="verkkokaupat" className="text-white font-bold text-lg">Verkkokaupat</a>
+        </li>
+        <li>
+          <a href="mobiilisovellukset" className="text-white font-bold text-lg">Mobiilisovellukset</a>
+        </li>
+        <li>
+          <a href="yhteystiedot" className="text-white font-bold text-lg">Yhteystiedot</a>
+        </li>
+      </ul>
+    </nav>
 
 
 
--- src/pages/verkkokaupat.tsx
+++ src/pages/verkkokaupat.tsx
@@ -3,9 +3,25 @@ import Head from "next/head";
 
 const VerkkokaupatPage: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-
-        <div>
+    
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <nav className="bg-gray-900 w-full">
+        <ul className="flex justify-end items-right h-full ml-auto">
+            <li>
+                <a href="/" className="text-white font-bold text-lg">Etusivu</a>
+            </li>
+            <li>
+                <a href="verkkosivut" className="text-white font-bold text-lg">Verkkokaupat</a>
+            </li>
+            <li>
+                <a href="mobiilisovellukset" className="text-white font-bold text-lg">Mobiilisovellukset</a>
+            </li>
+            <li>
+                <a href="yhteystiedot" className="text-white font-bold text-lg">Yhteystiedot</a>
+            </li>
+            </ul>
+    </nav>
+        <div className="flex min-h-screen flex-col items-center justify-center">
             <Head>
             <link rel="icon" href="/favicon.ico" />
         </Head>
